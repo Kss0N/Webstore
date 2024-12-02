6aafb149b485f440c7bfcf2803a1f6f3a0f3397e
Kss0N <ja6764kr-s@student.lu.se>
2024-12-02 23:53:28 +0100

    Added dark mode to front end

--- webstore.client/src/App.tsx
+++ webstore.client/src/App.tsx
@@ -1,12 +1,27 @@
 import { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+
 import './App.css';
 
+
+
+
+
+
+function getPreferredTheme() {
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+}
 
 export default function App() {
 
 
-  return (<>
-    <h1>Webshop</h1>
 
-  </>);
+
+  return (<Container data-bs-theme={getPreferredTheme()}>
+    <h1 className="display-1 border-bottom mb-2">Webshop</h1>
+
+    <footer className="footer">
+
+    </footer>
+  </Container>);
 };
