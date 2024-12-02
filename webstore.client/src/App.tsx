bf96e50a9a94bd60272906eb693412dc1ce927e9
Kss0N <ja6764kr-s@student.lu.se>
2024-12-02 23:53:28 +0100

    Added dark mode to front end

--- webstore.client/src/App.tsx
+++ webstore.client/src/App.tsx
@@ -1,12 +1,25 @@
 import { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+
 import './App.css';
+
+
+
+
+
+
 
 
 export default function App() {
 
 
-  return (<>
-    <h1>Webshop</h1>
 
-  </>);
+
+  return (<Container>
+    <h1 className="display-1 border-bottom mb-2">Webshop</h1>
+
+    <footer className="footer">
+
+    </footer>
+  </Container>);
 };
--- webstore.client/src/main.tsx
+++ webstore.client/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+
 import './index.scss'
 import App from './App.tsx'
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+// Dark mode for dark mode users.
+document.documentElement.setAttribute('data-bs-theme', window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
