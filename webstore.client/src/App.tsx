963d1bf2bdc9d9417b571ade586b86f529f1d527
Kss0N <ja6764kr-s@student.lu.se>
2024-12-04 22:06:28 +0100

    Added a Navbar

--- webstore.client/src/App.tsx
+++ webstore.client/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useReducer } from 'react';
-import { Container, ListGroup, Row } from 'react-bootstrap';
+import { Card, Carousel, Container, ListGroup, Nav, Navbar, Ratio, Row } from 'react-bootstrap';
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin, faXTwitter } from '@fortawesome/free-brands-svg-icons';
@@ -9,11 +9,82 @@ import './App.css';
 
 
 
+interface Offer {
+  name: string;
+  image: string;
+  price: number;
+  discount: number;
+};
+
+const myBestOffers: Offer[] = [
+  {
+    name: 'Blue shirt',
+    image: 'https://i5.walmartimages.com/asr/3c92c819-468f-4edd-8098-551a16ea9f1c.a8809b55a1038c8f51139443cd9e92db.jpeg',
+    price: 100,
+    discount: 10
+  },
+  {
+    name: 'sneakers',
+    image: 'https://thumblr.uniid.it/product/225766/97b4fbed16bd.jpg?width=1200&format=webp&q=75',
+    price: 150,
+    discount: 5
+  }
+];
+
+function BestOfferCard({ offer }: {offer: Offer}) {
+
+  return (<Card>
+    <Card.Img src={offer.image} variant="top" className="ratio ratio-2x1" />
+    <Card.Body>
+      <Card.Title>{offer.name}</Card.Title>
+    </Card.Body>
+
+  </Card>);
+}
+
+function BestOffers({ offers }: {offers: Offer[]}) {
+
+  return (<Carousel interval={2000}>
+    {
+      offers.map(offer =>
+      (<Carousel.Item key={offer.name}>
+
+        <BestOfferCard offer={offer} />
+
+      </Carousel.Item>))
+    }
+  </Carousel>);
+}
+
+function Toolbar({ isLoggedIn = false }: {isLoggedIn: boolean}) {
+
+  return (<Navbar sticky="top">
+
+    <Nav>
+      <Nav.Item>
+        <Nav.Link>
+          <i className="bi bi-person-circle"></i> {isLoggedIn ? "Account" : "Login" }
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link>
+          <i className="bi bi-cart3"></i> Cart
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link>
+          <i className="bi bi-gear"></i> Preferences
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+
+    
+  </Navbar>);
+}
 
 
 
-function Social({label="", link="", icon ="" }: {label:string, link: string, icon: string | IconDefinition })
-{
+function Social({label="", link="", icon ="" }: {label:string, link: string, icon: string | IconDefinition }) {
   return (<ListGroup.Item href={link} action target="_blank">
     {typeof icon == 'string' ? (<i className={icon}></i>) : (<FontAwesomeIcon icon={icon} />)} {label}
   </ListGroup.Item>);
@@ -24,11 +95,17 @@ export default function App() {
 
 
 
+
   return (<Container fluid>
+
+    <Toolbar/>
+
     <h1 className="display-1 border-bottom mb-2">Webshop</h1>
 
-    <main className="h-60">
-      <p>AAA</p>
+    <main className="min-vh-60">
+      <Container className="h-25 mb-10 d-inline-block d-flex justify-content-center">
+        <BestOffers offers={myBestOffers} />
+      </Container>
     </main>
 
     <footer className="footer border-top mt-2">
